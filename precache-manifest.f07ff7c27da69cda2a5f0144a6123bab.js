a2a70a7d08311abe0ec26922ec86d34bff66509d
Jsong576 <scsongc@163.com>
2019-06-14 03:47:27 -0400

    Add files via upload

---
+++ precache-manifest.f07ff7c27da69cda2a5f0144a6123bab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4c90a01456b584853dba2714946e7c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45275e4ed4af18bbda58",
+    "url": "/portfolio/static/css/main.fb43af2d.chunk.css"
+  },
+  {
+    "revision": "4c95f44f399b6150c266",
+    "url": "/portfolio/static/js/2.0007278a.chunk.js"
+  },
+  {
+    "revision": "45275e4ed4af18bbda58",
+    "url": "/portfolio/static/js/main.d1e2fbca.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c82d9387ad7309da712324e1a5d1f20a",
+    "url": "/portfolio/static/media/made_evolve_sans_thin_evo_personal_use-webfont.c82d9387.woff2"
+  },
+  {
+    "revision": "99f9316b509e49a201843ccb0f7308c4",
+    "url": "/portfolio/static/media/my-subaru.99f9316b.svg"
+  }
+]);
